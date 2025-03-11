c696219cb7ba0c849292da82b94d70fb85b47067
Alzter <alexsmall713@gmail.com>
2025-03-11 11:37:18 +1100

    Text classification LLM prompt

--- shell.nix
+++ shell.nix
@@ -1,41 +1,32 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+let
+    pkgs = import <nixpkgs> {config = {
+        enableCuda = false;
+        allowUnfree = true;
+    };};
+in
 pkgs.mkShell {
-    name="impurePythonEnv";
-    venvDir="./.venv";
-    packages = [
-
-        # Python Dependencies
-        (pkgs.python312.withPackages(pypkgs: with pypkgs; [
-
+    buildInputs = with pkgs; [
+        (python312.withPackages (p: with p; [
+            ipykernel
+            jupyter
             pip
             numpy
             pandas
             torch
             torchvision
             torchaudio
-            ipykernel
-            jupyter
+            tqdm
+#             bitsandbytes
+            transformers
+#             peft
+#             accelerate
             datasets
-
-        ]))
-
+#             scipy
+#             einops
+#             evaluate
+            #trl
+            #rouge_score
+        ]
+    ))
     ];
-
-    # SOURCE: https://www.reddit.com/r/NixOS/comments/1706v6p/comment/k3jry2z/
-    # Run this command, only after creating the virtual environment
-    postVenvCreation = ''
-        unset SOURCE_DATE_EPOCH
-
-        python -m ipykernel install --user --name=myenv4 --display-name="myenv4"
-        pip install -r requirements.txt
-    '';
-
-    # Now we can execute any commands within the virtual environment.
-    # This is optional and can be left out to run pip manually.
-    postShellHook = ''
-        # allow pip to install wheels
-        unset SOURCE_DATE_EPOCH
-    '';
-
 }
