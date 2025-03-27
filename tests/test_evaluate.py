99492ca2cbcbb859560b4319c7d5d3e610bb1424
Alzter <alexsmall713@gmail.com>
2025-03-27 15:29:53 +1100

    turns out the hf smol model doesn't have a chat template, lol

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -8,15 +8,15 @@ import torch
 
 @pytest.fixture
 def llm():
-    # bnb_config = BitsAndBytesConfig(
-    #     load_in_4bit = True,
-    #     bnb_4bit_quant_type = "nf4", # QLoRA uses 4-bit NormalFloat precision,
-    #     bnb_4bit_use_double_quant = True, # QLoRA uses double quantising,
-    #     bnb_4bit_compute_dtype = torch.float16
-    # )
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit = True,
+        bnb_4bit_quant_type = "nf4", # QLoRA uses 4-bit NormalFloat precision,
+        bnb_4bit_use_double_quant = True, # QLoRA uses double quantising,
+        bnb_4bit_compute_dtype = torch.float16
+    )
 
-    model_id = "HuggingFaceTB/SmolLM2-1.7B"
-    model = AutoModelForCausalLM.from_pretrained(model_id, device_map="cuda:0", torch_dtype=torch.bfloat16)
+    model_id = "Qwen/Qwen2.5-7B-Instruct"
+    model = AutoModelForCausalLM.from_pretrained(model_id, device_map="cuda:0", quantization_config=bnb_config)
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     return (model, tokenizer)
 
