309e0027c80ef41334eb5ae691c6f1ae0927c7a1
Alzter <alexsmall713@gmail.com>
2025-04-24 08:07:01 +1000

    osha multi-task prompt

--- src/model_prompts.py
+++ src/model_prompts.py
@@ -40,3 +40,32 @@ OSHA = {
     Read the following OSHA injury report then answer with the name of the category which suits it best.
     Answer with ONLY the name of the category, i.e. "Fractures"."""
 }
+OSHA = {
+    "ZERO_SHOT" : """You are an expert at classifying OSHA injury reports into the following categories:
+
+    CATEGORIES:
+    0. Amputations
+    1. Crushing injuries
+    2. Cuts, lacerations
+    3. Fractures
+    4. Heat (thermal) burns, unspecified
+    5. Internal injuries to organs and blood vessels of the trunk
+    6. Intracranial injuries, unspecified
+    7. Puncture wounds, except gunshot wounds
+    8. Soreness, pain, hurt-nonspecified injury
+    9. Traumatic injuries and disorders, unspecified
+
+    Read the following OSHA injury report then answer with the name of the category which suits it best.
+    Answer with ONLY the name of the category, i.e. "Fractures".""",
+
+    "MULTI_TASK" : """You are an expert at classifying OSHA injury reports.
+
+    You are given two classification tasks.
+    You should output the result as a two json fields as {"NatureTitle": "nature_title_label", "Part of Body Title": "part_of_body_title_label"}
+    For NatureTitle, given the injury report, you are asked to classify it as one of the labels in the list ["Amputations", "Crushing injuries", "Cuts, lacerations", "Fractures", "Heat (thermal) burns, unspecified", "Internal injuries to organs and blood vessels of the trunk", "Intracranial injuries, unspecified", "Puncture wounds, except gunshot wounds", "Soreness, pain, hurt-nonspecified injury", "Traumatic injuries and disorders, unspecified"] and change nature_title_label to the correct label in the list.
+    For Part of Body Title, given the injury report, you are asked to classify it as one of the labels in the list ["Ankle(s)", "BODY SYSTEMS", "Brain", "Finger(s), fingernail(s), n.e.c.", "Finger(s), fingernail(s), unspecified", "Fingertip(s)", "Hip(s)", "Leg(s), unspecified", "Multiple body parts, n.e.c.", "Nonclassifiable"] and change part_of_body_title_label to the correct label in the list.
+
+    Output the two json fields only and absolutely nothing else.
+    Now it is your turn.
+    """
+}
